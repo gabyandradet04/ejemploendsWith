9ec04d7e182466d004c9ab0a3872288153772e19
gabyandrade04 <gabrielandrade04@gmail.com>
2017-08-29 12:23:24 -0300

    Ejemplos endsWith

---
+++ src/Cadenas4/Cadenas41.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cadenas4;
+
+/**
+ *
+ * @author Gaby
+ */
+public class Cadenas41 {
+    public static void main(String[] args){
+        String cadena="buenas tardes profe";
+        
+        boolean b1= cadena.endsWith("profe");
+        System.out.println(b1);
+        boolean b2= cadena.endsWith("tardes");
+        System.out.println(b2);
+
+         
+    }
+    
+    
+    
+}
